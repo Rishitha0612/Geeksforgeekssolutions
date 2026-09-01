7bac85405bbe63ef1203eccecbd6b211fcc5c36c
Rishitha0612 <vemishettyrishitha@gmail.com>
2026-09-01 20:06:38 +0530

    Create MoveAllZerosToEnd.py

---
+++ MoveAllZerosToEnd.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pushZerosToEnd(self, arr):
+        pos=0
+        for num in arr:
+            if num!=0:
+                arr[pos]=num
+                pos+=1
+        while pos<len(arr):
+            arr[pos]=0
+            pos+=1
+        return arr
